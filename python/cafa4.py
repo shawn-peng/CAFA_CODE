718da244ea433f6f76f13ec52cf98e1a97230cb1
Yisu Peng <yisupeng@gmail.com>
2023-10-14 22:55:53 -0400

    cafa4.py

---
+++ python/cafa4.py
@@ -0,0 +1,21 @@
+
+species_map = {
+    '10090': ('Mus musculus', 'mouse'),
+    '10116': ('Rattus norvegicus', 'rat'),
+    #'226900': ('Bacillus cereus', ''),
+    '243273': ('Mycoplasma genitalium', ''),
+    '284812': ('Schizosaccharomyces pombe', ''),
+    #'287': ('Pseudomonas aeruginosa', ''),
+    '3702': ('Arabidopsis thaliana', 'arabidopsis'),
+    '44689': ('Dictyostelium discoideum', 'dicty'),
+    #'4577': ('Zea mays', ''),
+    '559292': ('Saccharomyces cerevisiae', 'yeast'),
+    #'6239': ('Caenorhabditis elegans', ''),
+    '7227': ('Drosophila melanogaster', 'fly'),
+    '7955': ('Danio rerio', 'zebrafish'),
+    '83333': ('Escherichia coli', ''),
+    '9606': ('Homo sapiens', 'human'),
+    #'9823': ('Sus scrofa', 'pig'),
+    '99287': ('Salmonella enterica subsp. enterica serovar Typhimurium str. LT2', ''),
+}
+
